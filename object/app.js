dd2923ee32f585c79bb5e4a95df49bcbd032a07a
Mrsurwa <134617790+Mrsurwa@users.noreply.github.com>
2023-08-07 20:46:54 +0500

    object

---
+++ object/app.js
@@ -0,0 +1,25 @@
+var object = {
+    name: 'urwa',
+    age: '24',
+    city: 'karachi',
+}
+console.log(object)
+
+var ArryObject = [
+    {
+        name: 'urwa',
+        age: '24',
+        city: 'karachi'
+    },
+    {
+        name: 'ubaid',
+        age: '14',
+        city: 'lahore'
+    },
+    {
+        name: 'balaj',
+        age: '1o',
+        city: 'islamad'
+    },
+]
+console.log(ArryObject[1].city)
